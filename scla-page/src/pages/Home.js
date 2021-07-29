43a35743caadd15d6ef1858173ea86d0b2333f13
Miguel <menjivarmiguel221@gmail.com>
2021-07-28 23:37:28 -0700

    managed to get landing page started with background and overlay, might change it around to get button instead of 2 images

--- scla-page/src/pages/Home.js
+++ scla-page/src/pages/Home.js
@@ -1,30 +1,49 @@
 import React, { Fragment } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+// images
+import TXT_logo from "../util/img/logo_TXT_color_lg_2014.02.05.jpg";
+import TXT_flyer from "../util/img/2021_DemoDay_Flyer 2.jpg";
 
 import "./Home.css";
-
-import { Container, Row, Col } from "react-bootstrap";
 
 const Home = () => {
   return (
     <Fragment>
-      <div id="jumbotron-title">
-        <h1 className="text-center">SCLA Demo Day 2021</h1>
-      </div>
-      <Container>
-        <div id="jumbotron-description">
-          <Row>
-            <Col>
-            <h1 className="text-center">TXT FOR LIFE EVER</h1>
+      <div className="overlay-background">
+        <div id="jumbotron-title">
+          <h1 className="text-center">SCLA Demo Day 2021</h1>
+        </div>
+
+        <Container>
+          <Row className="text-center">
+            <Col className="vertical-center">
+              {/* change this into a sign up button */}
+              <img src={TXT_logo} alt="Urban TXT logo" width="500" />
             </Col>
             <Col>
-                <h2>August 14th, 2021 10am-1pm</h2>
+              <img src={TXT_flyer} alt="SCLA Demo Day flyer" width="500" />
             </Col>
           </Row>
-          {/* <h1 className="text-center">TXT FOR LIFE EVER</h1> */}
-        </div>
-      </Container>
+        </Container>
+      </div>
     </Fragment>
   );
 };
 
 export default Home;
+
+// {/* <Container>
+// <div id="jumbotron-description">
+//   <Row className="text-center">
+//     <Col>
+//     <img src={TXT_logo} alt="Urban TXT logo" width="350"/>
+//     </Col>
+//     <Col>
+//         <h2>Saturday <br /> August 14th, 2021<br /> 10am-1pm</h2>
+//         <Button>Sign up!</Button>
+//     </Col>
+//   </Row>
+//   {/* <h1 className="text-center">TXT FOR LIFE EVER</h1> */}
+// </div>
+// </Container> */}
