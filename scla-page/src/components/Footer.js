00677abbf58a3643b8c9b4e0765145ddac6c5662
Miguel <menjivarmiguel221@gmail.com>
2021-07-30 21:56:13 -0700

    made changes, changed up the footer, added css to style it a bit.

--- scla-page/src/components/Footer.js
+++ scla-page/src/components/Footer.js
@@ -1,31 +1,45 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
+import { Instagram, Twitter, Facebook, Linkedin } from "react-bootstrap-icons";
 
 // image
-import TXT_logo from "../util/img/logo_TXT_color_lg_2014.02.05.jpg";
+import TXT_logo from "../util/img/logo_TXT_black_lg_2014.02.05.png";
+
+// css
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer>
       <Container>
-        <Row className="text-center">
-          <Col>
-            <img src={TXT_logo} alt="Urban TXT logo" width="100" />
+        <Row>
+          <Col className="text-center my-auto">
+            <img src={TXT_logo} alt="Urban TXT logo" width="150" />
           </Col>
-          <Col>
+          <Col className="text-center">
             <h3>Follow Us</h3>
-            <ul>
-                <li>LinekdIn</li>
-                <li>Twitter</li>
-                <li>Instagram</li>
-                <li>Facebook</li>
-            </ul>
+            <div>
+              <ul className="social-media">
+                <li>
+                  <Linkedin size={25} />
+                </li>
+                <li>
+                  <Twitter size={25} />
+                </li>
+                <li>
+                  <Instagram size={25} />
+                </li>
+                <li>
+                  <Facebook size={25} />
+                </li>
+              </ul>
+            </div>
           </Col>
-          <Col>
+          <Col className="text-center">
             <h3>Quick Links</h3>
-            <ul>
-                <li>sign up</li>
-                <li>Donate</li>
+            <ul className="quick-links">
+              <li>Sign up</li>
+              <li>Donate</li>
             </ul>
           </Col>
         </Row>
